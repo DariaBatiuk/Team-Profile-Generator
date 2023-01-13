8919761d224dc1c37a4dfdbc8dafaf78f3b9d016
Daria <dariabatiuk5@gmail.com>
2023-01-13 18:20:26 -0400

    added createHtml function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,6 @@
 const inquirer = require('inquirer');
+const fs = require('fs')
 const Manager = require('../Main/Manager');
 const Engineer = require('../Main/Engineer');
 const Intern = require('../Main/Intern');
+const createHtml = require('./createHtml');
