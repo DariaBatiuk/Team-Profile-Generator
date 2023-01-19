8cd30fda25a1f7a6ce048103645e450e818691e3
Daria <dariabatiuk5@gmail.com>
2023-01-18 20:36:25 -0400

    changing html str-re

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -25,26 +25,3 @@ describe("Getting data", () => {
 			expect(intern.getJob()).toBe('Intern');
 	});
 })
-
-// test('getting the Intern data for Intern object', () =>{
-// 	expect(intern.name).toBe('John');
-// 	expect(intern.id).toBe('i');
-// 	expect(intern.email).toBe('john@gmail.com');
-// 	expect(intern.school).toBe('MonctonHigh');
-// });
-
-// test('getting Intern name from getName()', () => {
-// 	expect(intern.getName().toBe('John'));
-// });
-// test('getting Intern id from getId()', () => {
-// 	expect(intern.getId().toBe('i'));
-// });
-// test('getting Intern email from getEmail()', () => {
-// 	expect(intern.getEmail().toBe('john@gmail.com'));
-// });
-// test('getting Intern number from getNumber()', () => {
-// 	expect(intern.getschool().toBe('MonctonHigh'));
-// });
-// test('getting Intern job from getJob()', () => {
-// 	expect(intern.getJob().toBe('Intern'));
-// });
